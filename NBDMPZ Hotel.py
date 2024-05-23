d07a681297b9a3e6f1f62d157f36ea0083aea11d
nbdmpz <nbdmpz@neptun.gde.hu>
2024-05-23 11:06:03 +0200

    ––

--- NBDMPZ Hotel.py
+++ NBDMPZ Hotel.py
@@ -1,5 +1,8 @@
+
+from datetime import datetime
+
 class Hotel:
-    def __init__(self,hotel_name ,hotel_address):
+    def __init__(self, hotel_name, hotel_address):
         self.hotel_name = hotel_name
         self.hotel_address = hotel_address
 
@@ -13,7 +16,7 @@ class Lakosztaly(Hotelszoba):
     def __init__(self, lakosztaly_tajolas, lakosztaly_erkely):
         super().__init__('lakosztaly')
         self.lakosztaly_tajolas = lakosztaly_tajolas
-        self.lakosztaly_erkely  = lakosztaly_erkely
+        self.lakosztaly_erkely = lakosztaly_erkely
 class standard(Hotelszoba):
     def __init__(self, hotelszoba_agyakszama):
         super().__init__('standard')
@@ -26,7 +29,7 @@ class foglalalas_osztaly:
         self.datum = datum
         self.vendegneve = vendegneve
 
-my_hotel = Hotel('NBDMPZ HOTEL', '1234 BP Egyetem tér 3')
+my_hotel = Hotel('NBDMPZ HOTEL', '1234 BP Egyetem ter 3')
 my_szoba1 = Lakosztaly('tengerre nezo','erkelyes')
 my_szoba1.hotelszoba_name = 'Kredit'
 my_szoba1.hotelszoba_price = 150000
@@ -47,12 +50,92 @@ my_szoba4.hotelszoba_name = 'ZH'
 my_szoba4.hotelszoba_price = 75000
 my_szoba4.hotelszoba_num = 7
 
-my_foglalas = foglalalas_osztaly( 'NBDMPZ Hotel',  10, '2024.08.10','Kiss Józsi')
-my_foglalas2 = foglalalas_osztaly ( 'NBDMPZ Hotel', 9, '2024.04.30.', 'Papp Pista' )
-my_foglalas3 = foglalalas_osztaly ( 'NBDMPZ Hotel', 10, '2024.07.30.', 'Papp Pista' )
-my_foglalas4 = foglalalas_osztaly ( 'NBDMPZ Hotel', 8, '2025.07.30.', 'Szabó Irén' )
-my_foglalas5 = foglalalas_osztaly ( 'NBDMPZ Hotel',  7 , '2025.08.11.', 'Tóth Erika' )
-my_foglalas6 foglalalas_osztaly ( 'NBDMPZ Hotel', 9, '2024.06.23.', 'Deák Boldizsárné' )
+my_foglalas = []
+my_foglalas.append(foglalalas_osztaly( 'NBDMPZ Hotel',  10, '2024.08.10','Kiss Jozsi'))
+my_foglalas.append(foglalalas_osztaly( 'NBDMPZ Hotel', 9, '2024.05.16', 'Papp Pista'))
+my_foglalas.append(foglalalas_osztaly( 'NBDMPZ Hotel', 10, '2024.07.30', 'Papp Pista'))
+my_foglalas.append(foglalalas_osztaly( 'NBDMPZ Hotel', 8, '2025.07.30', 'Szabó Iren'))
+my_foglalas.append(foglalalas_osztaly( 'NBDMPZ Hotel',  7 , '2025.08.11', 'Toth Erika'))
+my_foglalas.append(foglalalas_osztaly ( 'NBDMPZ Hotel', 9, '2024.06.23', 'Deak Boldizsarne'))
+
+
+
+def foglalt(datum3, szoba3):
+    foglalte=False
+    for foglalas in my_foglalas:
+        if datum3 == foglalas.datum and str(szoba3) == str(foglalas.szobaszam):
+           foglalte=True
+    return foglalte
+
+
+
+def listazas():
+    for foglalas in my_foglalas:
+        print("Nev: "+foglalas.vendegneve + " Szobaszam: "+str(foglalas.szobaszam) + " Datum: "+str(foglalas.datum))
+
+
+def ujfoglalas(vendeg, datum, szoba):
+    if foglalt(datum, szoba):
+        return print("Szoba foglalt")
+    else:
+        my_foglalas.append(foglalalas_osztaly( 'NBDMPZ Hotel',szoba, datum, vendeg))
+        if(szoba==10):
+            szobaar=my_szoba1.hotelszoba_price
+        elif(szoba==9):
+            szobaar=my_szoba2.hotelszoba_price
+        elif(szoba==8):
+            szobaar=my_szoba3.hotelszoba_price
+        elif(szoba==7):
+            szobaar=my_szoba4.hotelszoba_price
+        else:
+            szobaar= 0
+        return print("Foglalas letrehozva.Szobaar:" +str(szobaar))
+
+def lemondas(datum2, szoba2):
+    if foglalt(datum2, szoba2):
+        for foglalas in my_foglalas:
+            if datum2 == foglalas.datum and str(szoba2) == str(foglalas.szobaszam):
+                my_foglalas.remove(foglalas)
+        print("Szoba lemondva")
+    else:
+        return print("Nincs ilyen foglalas")
+
+
+
+def Menu():
+    while True:
+        print("\nVálassz Menupontot:")
+        print("1. Szoba foglalása")
+        print("2. Foglalás lemondása")
+        print("3. Foglalások listázása")
+        print("4. Kilépés")
+        menu = input("Valasztott Menupont: ")
+        if menu == "1":
+            nev=input("Kérem adja meg a nevét:")
+            szobasz = input("Szobaszam: ")
+            datum = input("Foglalas datuma (EEEE.MM.DD) formátumban: ")
+            datum1 = datetime.strptime(datum, "%Y.%m.%d")
+            if datum1 < datetime.now():
+                print("\nRossz dátum! A foglalás csak jövőbeni időpontra lehetséges.")
+            elif szobasz != "7" and szobasz != "8" and szobasz != "9" and szobasz != "10":
+                print("Nincs ilyen szoba")
+            else:
+                ujfoglalas(nev,datum, int(szobasz))
+        elif menu == "2":
+            szobasz = input("Adja meg a lemondani kívánt szobaszamot: ")
+            datum = input("Foglalas datuma (EEEE.MM.DD) formátumban: ")
+            lemondas(datum,int(szobasz))
+        elif menu == "3":
+            listazas()
+        elif menu == "4":
+            break
+
+        else:
+            print("Hibás menupont")
+
+
+lemondas('2024.07.30', '10')
+Menu()
 
 
 
@@ -67,12 +150,6 @@ my_foglalas6 foglalalas_osztaly ( 'NBDMPZ Hotel', 9, '2024.06.23.', 'Deák Boldi
 
 
 
-
-
-my_lakosztaly= Hotelszoba("Lakosztály")
-my_lakosztaly.hotelszoba_name = " CSILLAG "
-my_lakosztaly.hotelszoba_price = 15000
-my_lakosztaly.hotelszoba_num = 10
 
 
 
